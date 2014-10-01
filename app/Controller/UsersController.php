cc2fd1b0f5d96149ff5982f622acf8ae76c8ae86
Loy Ramirez <loyenrique1@gmail.com>
2014-10-01 17:15:50 -0400

    emails key test

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends AppController {
 	
 	 public function beforeFilter() {
         parent::beforeFilter();
-        $this->Auth->allow('add');
+        $this->Auth->allow('add','active_account');
     }
 
     public function index() {
